ea06506d816aedfbf85927920bc67a81eade334a
Jan Küfner <jan.kuefner@gmx.de>
2021-12-04 10:41:37 +0100

    thermostat sending stuff

--- thermostat/app.py
+++ thermostat/app.py
@@ -1,42 +1,15 @@
 import paho.mqtt.client as mqtt
-import time #used to code sleep times for devices to safe battery
-
-username = input("press ENTER to continue:")
-print("weiter geht es")
+import time
 
 
-
-
-import os  
-
-hostname = "nebula_mosquitto_container" #example
-#hostname = "localhost" #example
-response = os.system("ping -c 1 " + hostname)
-
-#and then check the response...
-if response == 0:
-  print ('broker is up!')
-else:
-  print ('broker is down!')
-
-
-print("Yolo") 
-print("###")
-print("###")
-print("###")
-print("###")
-
+any_var = input("connect node to the Docker network - and then press Enter")
+print("moving on...")
 
 client = mqtt.Client()
-#client.connect("localhost",6666,60)
-client.connect("nebula_mosquitto_container",1883,60)
-client.publish("/test", "yolo")
-  
+client.connect("nebula_mosquitto_container",1883,60) #connect to broker
 
 
-n=0
-while True:
-  n=n+1
-  time.sleep(5)
-  print (n)
+while True: #loop forever
+  time.sleep(3) #device sleeps to save battery power
+  client.publish("/temperature", "yolo")
   
