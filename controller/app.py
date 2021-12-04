6c4f2bb52af70aa7300c9a422ad397aeb1d54dce
Jan Küfner <jan.kuefner@gmx.de>
2021-12-04 13:05:14 +0100

    onedirectional communication working

--- controller/app.py
+++ controller/app.py
@@ -1,6 +1,11 @@
 import paho.mqtt.client as mqtt
 import time
 
+def on_message(client, userdata, msg):
+      print(msg.payload.decode())
+      
+      
+
 
 any_var = input("integrate this node to the Docker network & then press Enter")
 print("starting MQTTS...")
@@ -10,7 +15,12 @@ client.connect("nebula_mosquitto_container",1883,60) #connect to broker
 print("MQTTS started")
 
 
+
+client.subscribe("home/temp")
 while True: #loop forever
-  time.sleep(3) #communication sleeps to save battery power
-  client.publish("home/temp", "yolo")
+  #time.sleep(1) #communication sleeps to save battery power
+  #print("controller here, still working...")
+  
+  client.on_message = on_message
+  client.loop_start()
   
