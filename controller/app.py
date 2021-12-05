3724e202ad880a82e5aa3bde3d79512a5d690d6b
Jan Küfner <jan.kuefner@gmx.de>
2021-12-05 11:55:58 +0100

    controller havin OOP elements and a random ID

--- controller/app.py
+++ controller/app.py
@@ -1,24 +1,48 @@
 import paho.mqtt.client as mqtt
 import time
+import random #used to create ID
+
+
+class Controller:
+      def __init__(self, node_IDs):
+            '''node_IDs is an array that stores all IDs of the nodes'''
+            self.node_IDs = node_IDs
+            
+      def update_temperature_from_nodes(self):
+            '''this methods updates temperatures from nodes and prints them'''
+            print("Yolo")
+
+def create_ID():
+      return("cont"+str(random.randrange(1001,9999)))
 
 def on_message(client, userdata, msg):
-      print(msg.payload.decode())      
+      print(msg.payload.decode())
       
 
+controller = Controller([]) #create a Controller object
+client_id = create_ID() #create a random client ID
+client = mqtt.Client(client_id) #create a MQTT client object
 
+
+'''the following line of code is used to delay this programm / the node till 
+the user connected this node to the Docker network. This is essential, since a 
+MQTT connection to the broker can only be successfull, if the node is in the 
+same network as the broker'''
 any_var = input("integrate this node to the Docker network & then press Enter")
-print("starting MQTTS...")
 
-client = mqtt.Client()
+
 client.connect("nebula_mosquitto_container",1883,60) #connect to broker
 print("MQTTS started")
 
 
-
 client.subscribe("home/temp")
 while True: #loop forever
-  #time.sleep(1) #communication sleeps to save battery power
-  #print("controller here, still working...")
-  
+  time.sleep(1) #communication sleeps to save battery power
   client.on_message = on_message
   client.loop_start()
+  #print(client._client_id.decode())
+'''
+
+while True:
+      print(create_ID())
+      '''
