3d37e282d0c8c7b306d1dfaa43f0223996c8ce26
Jan Küfner <jan.kuefner@gmx.de>
2021-12-05 12:24:29 +0100

    thermo sends payload as desired

--- controller/app.py
+++ controller/app.py
@@ -3,6 +3,13 @@ import time
 import random #used to create ID
 
 
+def create_ID():
+      return("cont"+str(random.randrange(1001,9999)))
+
+def on_message(client, userdata, msg):
+      print(msg.payload.decode())
+      
+
 class Controller:
       def __init__(self, node_IDs):
             '''node_IDs is an array that stores all IDs of the nodes'''
@@ -11,13 +18,10 @@ class Controller:
       def update_temperature_from_nodes(self):
             '''this methods updates temperatures from nodes and prints them'''
             print("Yolo")
-
-def create_ID():
-      return("cont"+str(random.randrange(1001,9999)))
-
-def on_message(client, userdata, msg):
-      print(msg.payload.decode())
-      
+            client.subscribe("home/temp")
+            client.on_message = on_message
+            client.loop_start()
+                 
 
 controller = Controller([]) #create a Controller object
 client_id = create_ID() #create a random client ID
@@ -35,12 +39,10 @@ client.connect("nebula_mosquitto_container",1883,60) #connect to broker
 print("MQTTS started")
 
 
-client.subscribe("home/temp")
+
 while True: #loop forever
   time.sleep(1) #communication sleeps to save battery power
-  client.on_message = on_message
-  client.loop_start()
-  #print(client._client_id.decode())
+  controller.update_temperature_from_nodes()
 '''
 
 while True:
--- thermostat/app.py
+++ thermostat/app.py
@@ -3,14 +3,16 @@ import time #used to create a timer
 import os #used to clear screen in Gui class
 import random #used to create a random number
 
-
+def create_ID():
+      return("temp"+str(random.randrange(1001,9999)))
 
 class Thermometer:
-      def __init__(self, set_temperature, current_temperature):
+      def __init__(self, set_temperature, current_temperature, payload):
         '''set_temperature stores the value given by the controller the  
         heating is set to'''
         self.set_temperature = set_temperature
         self.current_temperature = current_temperature
+        self.payload = payload
         
       def render_gui(self):
         '''this methods renders a GUI of a thermometer'''
@@ -18,17 +20,19 @@ class Thermometer:
         print("Current temperature: ", self.current_temperature)
         print("----------------------------")
         
-      def measure_temperature(self):
-        '''this method simulates that the measured temperature is sometimes not 
-        as the set temperature.'''
+      def create_payload(self):
+        '''the following if statement is to simulate that the measured 
+        temperature is sometimes not as the set temperature.'''
         if random.randrange(0,5) == 3:
           self.current_temperature = self.set_temperature +1
         else:
           self.current_temperature = self.set_temperature
+        self.payload = client_id + "actual--" + str(self.set_temperature)
             
       
-thermometer = Thermometer (20, 13) #create a thermometer object with some values
+client_id = create_ID() #create a random client ID
 client = mqtt.Client() #create a MQTT client object
+thermometer = Thermometer (20, 13, "lalala") #create a thermometer object
 
 
 '''the following line of code is used to delay this programm / the node till 
@@ -42,8 +46,10 @@ client.connect("nebula_mosquitto_container",1883,60) #connect to broker
 
 
 while True: #loop forever
-  time.sleep(3) #communication sleeps to save battery power
-  thermometer.measure_temperature() #get a temp measurement
-  client.publish("home/temp", thermometer.current_temperature) #publish tempe
+  '''communication sleeps to save battery power. A random value for sleeping 
+  was chosen to simulate an unreliable node'''
+  time.sleep(random.randrange(1,4)) 
+  thermometer.create_payload() #create payload
+  client.publish("home/temp", thermometer.payload) #publish payload
   thermometer.render_gui() #render the gui
   
