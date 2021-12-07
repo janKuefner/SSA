e59cd896f25a6be787b33dc9fa6bee75b2ee990f
Jan Küfner <jan.kuefner@gmx.de>
2021-12-07 21:03:02 +0100

    wrong keys etc. deleted

--- simple_TLS_node/app.py
+++ simple_TLS_node/app.py
@@ -3,7 +3,6 @@ import time #used to simulate IoT delays
 import ssl
 
 print("simple TLS app")
-
     
 def on_message(client, userdata, msg):
       print(msg.payload.decode()) 
@@ -15,25 +14,25 @@ def on_log(client, userdata, level, buf):
  
 client = mqtt.Client() #create a MQTT client object
 
-
 any_var = input("integrate this node to the Docker network & then press Enter")
 
-
-
-
-
-
+'''
+while True:
+      print("yolo")
+      time.sleep(2)
+'''
 
 
 '''callback functions'''
 client.on_message = on_message #check for message from broker
 client.on_log=on_log
 '''TLS parameters'''
-#client.tls_set("/certs/broker.crt", tls_version=ssl.PROTOCOL_TLSv1_2)
-client.tls_set("/certs/broker.crt")
+client.tls_set("/app/certs/ca.crt", tls_version=ssl.PROTOCOL_TLSv1_2)
+#client.tls_set("/app/certs/client.crt")
 client.tls_insecure_set(True)
 '''TLS connect, subscribe'''
-client.connect("nebula_mosquitto_container",8883,60) #connect to broker 
+client.connect("LAPTOP-J6SF4LLV",8883,60) #connect to broker 
+client.connect("nebula_mosquitto_container",8883,60) #connect to broker
 client.subscribe("home/temp")
 print("MQTTS started") 
 '''publish'''
