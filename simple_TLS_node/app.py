f5a188be735eb514a009ff45f0d05b3743ced4e6
Jan Küfner <jan.kuefner@gmx.de>
2021-12-08 15:57:28 +0100

    certs now with Common Name localhost

--- simple_TLS_node/app.py
+++ simple_TLS_node/app.py
@@ -1,46 +1,43 @@
-import paho.mqtt.client as mqtt #used for MQTT protocol
+import paho.mqtt.client as paho #used for MQTT protocol
 import time #used to simulate IoT delays
 import ssl
 
-print("simple TLS app")
-    
-def on_message(client, userdata, msg):
-      print(msg.payload.decode()) 
-      
-      
-def on_log(client, userdata, level, buf):
-      print("log: ",buf)
-            
+
+           
  
-client = mqtt.Client() #create a MQTT client object
+client = paho.Client() #create a MQTT client object
 
 any_var = input("integrate this node to the Docker network & then press Enter")
 
 
-while True:
-      print("yolo")
-      time.sleep(2)
+
+
+'''
+
+
+client.tls_set("/app/certs/ca.crt", tls_version=ssl.PROTOCOL_TLSv1_1)
+client.tls_insecure_set(True) 
 
 
 
-'''callback functions'''
-client.on_message = on_message #check for message from broker
-client.on_log=on_log
-'''TLS parameters'''
-'''https://pypi.org/project/paho-mqtt/
-#tls_set(ca_certs=None, certfile=None, keyfile=None, cert_reqs=ssl.CERT_REQUIRED,
-    tls_version=ssl.PROTOCOL_TLS, ciphers=None)'''
-client.tls_set("/app/certs/ca.crt", tls_version=ssl.PROTOCOL_TLSv1_2)
-#client.tls_set("/app/certs/client.crt")
-client.tls_insecure_set(True)
-'''TLS connect, subscribe'''
-client.connect("LAPTOP-J6SF4LLV",8883,60) #connect to broker 
 client.connect("nebula_mosquitto_container",8883,60) #connect to broker
-client.subscribe("home/temp")
-print("MQTTS started") 
-'''publish'''
-client.publish("home/temp", "yolo") #publish payload
-'''MQTT LOOP'''
-client.loop_start() #loops MQTT commands
-time.sleep(5) #used to simulate a IoT device, since sleeping safes battery :)
+time.sleep(3)
+
+while True:
+      client.publish("home/temp", "yolo") #publish payload
+      time.sleep(1)
+
+
+'''
+
+
+
+
+
+client.connect("nebula_mosquitto_container",1883,60) #connect to broker
+time.sleep(3)
+
+while True:
+      client.publish("home/temp", "yolo") #publish payload
+      time.sleep(10)
   
