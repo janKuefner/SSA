f30b95e9037a50713ccc49b0868e1a98c51db59e
root <root@LAPTOP-J6SF4LLV.localdomain>
2021-12-08 14:36:51 +0100

    certs with nebula_mosquitto_container as Commonm Name for broker

--- simple_TLS_node/app.py
+++ simple_TLS_node/app.py
@@ -16,17 +16,20 @@ client = mqtt.Client() #create a MQTT client object
 
 any_var = input("integrate this node to the Docker network & then press Enter")
 
-'''
+
 while True:
       print("yolo")
       time.sleep(2)
-'''
+
 
 
 '''callback functions'''
 client.on_message = on_message #check for message from broker
 client.on_log=on_log
 '''TLS parameters'''
+'''https://pypi.org/project/paho-mqtt/
+#tls_set(ca_certs=None, certfile=None, keyfile=None, cert_reqs=ssl.CERT_REQUIRED,
+    tls_version=ssl.PROTOCOL_TLS, ciphers=None)'''
 client.tls_set("/app/certs/ca.crt", tls_version=ssl.PROTOCOL_TLSv1_2)
 #client.tls_set("/app/certs/client.crt")
 client.tls_insecure_set(True)
