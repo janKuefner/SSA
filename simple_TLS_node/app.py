2b77bf407e923ba9ebb49bd62459a0dce24b50e8
Jan Küfner <jan.kuefner@gmx.de>
2021-12-13 11:31:48 +0100

    minor changes

--- simple_TLS_node/app.py
+++ simple_TLS_node/app.py
@@ -1,43 +1,11 @@
-import paho.mqtt.client as paho #used for MQTT protocol
-import time #used to simulate IoT delays
-import ssl
-
-
+import paho.mqtt.client as paho 
+import time 
            
- 
-client = paho.Client() #create a MQTT client object
+client = paho.Client() 
+any_var = input() #here I connect this container to the docker network
 
-any_var = input("integrate this node to the Docker network & then press Enter")
-
-
-
-
-'''
-
-
-client.tls_set("/app/certs/ca.crt", tls_version=ssl.PROTOCOL_TLSv1_1)
+client.tls_set("/app/certs/ca.crt")
 client.tls_insecure_set(True) 
-
-
-
-client.connect("nebula_mosquitto_container",8883,60) #connect to broker
-time.sleep(3)
-
-while True:
-      client.publish("home/temp", "yolo") #publish payload
-      time.sleep(1)
-
-
-'''
-
-
-
-
-
-client.connect("nebula_mosquitto_container",1883,60) #connect to broker
-time.sleep(3)
-
-while True:
-      client.publish("home/temp", "yolo") #publish payload
-      time.sleep(10)
-  
+client.connect("nebula_mosquitto_container",8883,60) 
+time.sleep(5)
+client.publish("home/temp", "yolo") 
